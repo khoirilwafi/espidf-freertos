d701713f1e9ca3646d0c383aaebc10d5bb0f634f
khoirilwafi <68954908+khoirilwafi@users.noreply.github.com>
2023-10-02 03:04:58 +0700

    belajar queue

--- main/main.c
+++ main/main.c
@@ -1,90 +1,55 @@
 #include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+#include "math.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#include "driver/uart.h"
+#include "freertos/queue.h"
 
-uint16_t delay_value = 500;
+static QueueHandle_t msg_queue;
 
-void serial_task(void *parameter)
+void sender_task(void *parameter)
 {
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_DEFAULT,
-    };
-
-    uart_param_config(UART_NUM_0, &uart_config);
-    uart_set_pin(UART_NUM_0, 2, 3, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-    uart_driver_install(UART_NUM_0, 1024, 0, 0, NULL, 0);
-
-    int data_length = 0;
-    uint8_t *read_buffer = (uint8_t *)pvPortMalloc(1024);
-
-    char index_command[6];
-    char delay_command[6];
-
-    if (read_buffer == NULL)
-    {
-        printf("gagal membuat alokasi memori uart. menghapus task\r\n");
-        vTaskDelete(NULL);
-    }
+    int number = 0;
 
     while (1)
     {
-        data_length = uart_read_bytes(UART_NUM_0, read_buffer, 1024, pdMS_TO_TICKS(100));
+        number = (int)random();
 
-        if (data_length > 0)
+        if (xQueueSend(msg_queue, (void *)&number, 0) == pdFALSE)
         {
-            read_buffer[data_length] = '\0';
-            printf("%d karakter diterima: %s\r\n", data_length, read_buffer);
-
-            if (data_length > 6)
-            {
-                strncpy(index_command, (const char *)read_buffer, 5);
-                index_command[5] = '\0';
-
-                if (strcasecmp(index_command, "delay") == 0)
-                {
-                    int index = 6;
-
-                    while (read_buffer[index] != '\0')
-                    {
-                        delay_command[index - 6] = read_buffer[index];
-                        index++;
-                    }
-
-                    delay_command[index - 6] = '\0';
-
-                    delay_value = atoi(delay_command);
-                    printf("delay diperbarui menjadi %d\r\n", delay_value);
-                }
-            }
+            printf("antrian penuh\r\n");
         }
+        else
+        {
+            printf("%d berhasil terkirim\r\n", number);
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
-void blink_task(void *parameter)
+void receiver_task(void *parameter)
 {
-    gpio_set_direction(23, GPIO_MODE_OUTPUT);
-    printf("blink with initial delay value\r\n");
+    int recv_number = 0;
 
     while (1)
     {
-        gpio_set_level(23, 1);
-        vTaskDelay(pdMS_TO_TICKS(delay_value));
-        gpio_set_level(23, 0);
-        vTaskDelay(pdMS_TO_TICKS(delay_value));
+        if (xQueueReceive(msg_queue, &recv_number, 0) == pdTRUE)
+        {
+            printf("mendapatkan data dari antrian : %d\r\n", recv_number);
+        }
+        else
+        {
+            printf("antrian kosong\r\n");
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 
 void app_main(void)
 {
-    xTaskCreate(serial_task, "serial task", 2048, NULL, 1, NULL);
-    xTaskCreate(blink_task, "blink task", 1024, NULL, 1, NULL);
+    msg_queue = xQueueCreate(10, sizeof(int));
+
+    xTaskCreate(sender_task, "sender", 2048, NULL, 1, NULL);
+    xTaskCreate(receiver_task, "receiver", 2048, NULL, 1, NULL);
 }
